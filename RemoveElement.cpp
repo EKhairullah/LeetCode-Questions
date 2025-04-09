6dda5bdb90fd10ddc4ff0de017d53b3b14a34612
Khairullah <khairullahebrahimi120@gmail.com>
2025-04-09 21:04:40 +0330

    That was easy

--- RemoveElement.cpp
+++ RemoveElement.cpp
@@ -1,45 +1,78 @@
 #include <iostream>
-#include<vector>
+#include <vector>
 using namespace std;
-class Solution{
+
+// Definition for singly-linked list.
+  struct ListNode {
+      int val;
+     ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+class Solution {
     public:
-    int RemoveElement(vector<int> &array,int target)
-    {
-        if(array.size()==0) return 0;
-        int k = 0;
-        for(int i =0;i<array.size();i++)
-        {
-            if(array[i]!=target)
+        ListNode* removeElements(ListNode* head, int value) {
+            if(head ==nullptr) return head;
+            while(head != nullptr &&head->val == value) {
+                head = head->next;
+            }
+            ListNode * current = head;
+            while(current != nullptr &&current->next !=nullptr)
             {
-                array[k]=array[i];
-                k++;
+                if(current->next->val==value){
+                current->next=current->next->next;
+                
             }
             
+            else {
+                current =  current->next;
+            }
+
         }
-        return k;
+        return head;
     }
-};
+    
+    };
+    void append(ListNode*& head, int val) {
+        if (!head) {
+            head = new ListNode(val);
+            return;
+        }
+        ListNode* current = head;
+        while (current->next) {
+            current = current->next;
+        }
+        current->next = new ListNode(val);
+    }
+    void display(ListNode * head)
+    {
+        while (head !=nullptr)
+
+        {
+            cout<<head->val;
+            if(head->next != nullptr) cout<<"->";            
+            head = head->next;
+        }
+        cout<<"NULL";
+        
+    }
+
 int main()
 {
-    int Number;
-    cin>>Number;
-    int input;
-    int targ;
-    cout<<"enter your target: ";
-    cin>>targ;
-    vector <int> arr;
-    for(int i =0;i<Number;i++)
-    {
-        cin>>input;
-        arr.push_back(input);
-
-    }
-    cout<<"the size of array is: "<<arr.size()<<endl;
+    ListNode *head = nullptr;
+    append(head,1);
+    append(head,2);
+    
+    cout<<"before Delete: ";
+    display(head);
+    cout<<"\n After Delete: ";
     Solution s;
-    int k =s.RemoveElement(arr,targ);
-    cout<<k;
+    s.removeElements(head,1);
+    display(head);
+    return 0;
+    
 }
-
 
 // Input: nums = [0,1,2,2,3,0,4,2], val = 2
 // Output: 5, nums = [0,1,4,0,3,_,_,_]
